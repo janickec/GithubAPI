99c3aaede6afaabf04ba3cf52ebcc2f00fcd2873
cjaenick <callum.janicke@gmail.com>
2019-11-25 13:17:46 +0100

    Receive repositories from Github User with most commits

--- src/GithubAccess.java
+++ src/GithubAccess.java
@@ -4,12 +4,15 @@ import org.eclipse.egit.github.core.service.RepositoryService;
 
 
 public class GithubAccess{
+	public static void main (String [] args) {
 	GitHubClient client = new GitHubClient();
-
-client.setOAuth2Token("393a0233fd47f46e8873d7b87f9f9a3de6c15020");
+    client.setOAuth2Token("393a0233fd47f46e8873d7b87f9f9a3de6c15020");
+	getRepos();
+	}
+	
+public static void getRepos() {
 RepositoryService service = new RepositoryService();
-for (Repository repo : service.getRepositories("defunkt"))
+for (Repository repo : service.getRepositories("fabpot"))
   System.out.println(repo.getName() + " Watchers: " + repo.getWatchers());
-
 }
 }
